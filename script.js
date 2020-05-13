0cfed53ceeabf229feb820ffc805075b5e17b904
Pranoy Sarkar <pranoy.sarkar@softwareag.com>
2020-05-13 22:32:31 +0530

    added transaction history

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-console.log('hello world')
+
+let transactions=[];
 
 
 
-let h1 = document.createElement('h1');
-h1.innerHTML = "Hello world";
-console.log(h1);
 
-/* let body = document.querySelector('body');
-body.append(h1); */
+
+
 
 function createRow(name,type,amount) {
     let row = document.createElement('tr');
@@ -30,16 +28,62 @@ function createRow(name,type,amount) {
 
 }
 
-
-
-
 function addNewButtonClicked(){
-    /**
-     * Access data entered by user 
-     * and pass it
-     */
-    createRow('Pranoy','Debt','200 inr');
+    let name= document.querySelector('#name').value;
+    let type= document.querySelector('#payType').value;
+    let amount=  document.querySelector('#amount').value;
+    if(checkIfNameExists(name)){
+        alert('Name already exists');
+        return;
+    }
+
+    let transaction={
+        name:name,
+        type:type,
+        amount:amount
+    }
+    transactions.push(transaction);
+    updatePaymentUsers(name);
+    createRow(name, type, amount);
+}
+
+function updatePaymentUsers(name){
+    let opt= document.createElement('option');
+    opt.innerHTML=name;
+    let payNames= document.querySelector('#payNames');
+    payNames.append(opt);
+}
+
+function checkIfNameExists(name){
+    let result = false;
+    for(let i=0;i<transactions.length;i++){
+        let transaction = transactions[i];
+        if(transaction.name==name){
+            result = true;
+            return result;
+        }
+    }
+    return result;
 }
 
 let addNewButton= document.querySelector('#addNew');
 addNewButton.addEventListener('click',addNewButtonClicked)
+
+
+function createNewrow (name,amount) {
+    let row = document.createElement('tr');
+
+    let nameTd = document.createElement('td');
+    nameTd.innerHTML = name;
+    row.append(nameTd);
+
+    let priceTd = document.createElement('td');
+    priceTd.innerHTML = amount;
+    row.append(priceTd);
+
+    let tbody = document.querySelector('tbody');
+    tbody.append(row);
+}
+
+function addPayment () {
+}
